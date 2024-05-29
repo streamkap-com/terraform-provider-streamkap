0cdb86ea2679760d070760364248ff8e6b3bb611
Sau Phung <sau@streamkap.com>
2024-05-29 16:01:34 +0700

    feat: Add test for Snowflake destination

--- internal/provider/source_postgresql_test.go
+++ internal/provider/source_postgresql_test.go
@@ -8,8 +8,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-var dbHostname = os.Getenv("SOURCE_POSTGRESQL_HOSTNAME")
-var dbPassword = os.Getenv("SOURCE_POSTGRESQL_PASSWORD")
+var sourcePostgreSQLHostname = os.Getenv("SOURCE_POSTGRESQL_HOSTNAME")
+var sourcePostgreSQLPassword = os.Getenv("SOURCE_POSTGRESQL_PASSWORD")
 
 func TestAccSourcePostgreSQLResource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
@@ -19,32 +19,32 @@ func TestAccSourcePostgreSQLResource(t *testing.T) {
 			{
 				Config: providerConfig + fmt.Sprintf(`
 resource "streamkap_source_postgresql" "test" {
-    name                                         = "test-source-postgresql"
-    database_hostname                            = "%s"
-    database_port                                = 5432
-    database_user                                = "postgresql"
-    database_password                            = "%s"
-    database_dbname                              = "postgres"
-    database_sslmode                             = "require"
-    schema_include_list                          = "public"
-    table_include_list                           = "public.users"
-    signal_data_collection_schema_or_database    = "streamkap"
-    heartbeat_enabled                            = false
-    heartbeat_data_collection_schema_or_database = null
-    include_source_db_name_in_table_name         = false
-    slot_name                                    = "streamkap_pgoutput_slot"
-    publication_name                             = "streamkap_pub"
-    binary_handling_mode                         = "bytes"
-    ssh_enabled                                  = false
+	name                                         = "test-source-postgresql"
+	database_hostname                            = "%s"
+	database_port                                = 5432
+	database_user                                = "postgresql"
+	database_password                            = "%s"
+	database_dbname                              = "postgres"
+	database_sslmode                             = "require"
+	schema_include_list                          = "public"
+	table_include_list                           = "public.users"
+	signal_data_collection_schema_or_database    = "streamkap"
+	heartbeat_enabled                            = false
+	heartbeat_data_collection_schema_or_database = null
+	include_source_db_name_in_table_name         = false
+	slot_name                                    = "streamkap_pgoutput_slot"
+	publication_name                             = "streamkap_pub"
+	binary_handling_mode                         = "bytes"
+	ssh_enabled                                  = false
 }
-`, dbHostname, dbPassword),
+`, sourcePostgreSQLHostname, sourcePostgreSQLPassword),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify if attributes are propagated correctly
 					resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "name", "test-source-postgresql"),
-          resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_hostname", dbHostname),
+          resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_hostname", sourcePostgreSQLHostname),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_port", "5432"),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_user", "postgresql"),
-          resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_password", dbPassword),
+          resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_password", sourcePostgreSQLPassword),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_dbname", "postgres"),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "schema_include_list", "public"),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "table_include_list", "public.users"),
@@ -63,31 +63,31 @@ resource "streamkap_source_postgresql" "test" {
 			{
 				Config: providerConfig + fmt.Sprintf(`
 resource "streamkap_source_postgresql" "test" {
-    name                                         = "test-source-postgresql-updated"
-    database_hostname                            = "%s"
-    database_port                                = 5432
-    database_user                                = "postgresql"
-    database_password                            = "%s"
-    database_dbname                              = "postgres"
-    database_sslmode                             = "require"
-    schema_include_list                          = "public"
-    table_include_list                           = "public.users"
-    signal_data_collection_schema_or_database    = "streamkap"
-    heartbeat_enabled                            = false
-    include_source_db_name_in_table_name         = false
-    slot_name                                    = "streamkap_pgoutput_slot"
-    publication_name                             = "streamkap_pub"
-    binary_handling_mode                         = "bytes"
-    ssh_enabled                                  = false
+	name                                         = "test-source-postgresql-updated"
+	database_hostname                            = "%s"
+	database_port                                = 5432
+	database_user                                = "postgresql"
+	database_password                            = "%s"
+	database_dbname                              = "postgres"
+	database_sslmode                             = "require"
+	schema_include_list                          = "public"
+	table_include_list                           = "public.users"
+	signal_data_collection_schema_or_database    = "streamkap"
+	heartbeat_enabled                            = false
+	include_source_db_name_in_table_name         = false
+	slot_name                                    = "streamkap_pgoutput_slot"
+	publication_name                             = "streamkap_pub"
+	binary_handling_mode                         = "bytes"
+	ssh_enabled                                  = false
 }
-`, dbHostname, dbPassword),
+`, sourcePostgreSQLHostname, sourcePostgreSQLPassword),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify if attributes are propagated correctly
 					resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "name", "test-source-postgresql-updated"),
-          resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_hostname", dbHostname),
+          resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_hostname", sourcePostgreSQLHostname),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_port", "5432"),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_user", "postgresql"),
-          resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_password", dbPassword),
+          resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_password", sourcePostgreSQLPassword),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "database_dbname", "postgres"),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "schema_include_list", "public"),
           resource.TestCheckResourceAttr("streamkap_source_postgresql.test", "table_include_list", "public.users"),
