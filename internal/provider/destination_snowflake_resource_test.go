9bfa36c7126b45ff0ddbfa0f0e81e68b517c4a54
Sau Phung <sau@streamkap.com>
2025-04-15 10:59:21 +0000

    fix: add auto_qa_dedupe_table_mapping and update docs snowflake destination

--- internal/provider/destination_snowflake_resource_test.go
+++ internal/provider/destination_snowflake_resource_test.go
@@ -49,6 +49,10 @@ resource "streamkap_destination_snowflake" "test" {
 	apply_dynamic_table_script       = false
 	dynamic_table_target_lag         = 60
 	cleanup_task_schedule            = 120
+	auto_qa_dedupe_table_mapping = {
+		users                   = "JUNIT.USERS",
+		itst_scen20240528103635 = "ITST_SCEN20240528103635"
+	}
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
@@ -70,6 +74,8 @@ resource "streamkap_destination_snowflake" "test" {
 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "cleanup_task_schedule", "120"),
 					resource.TestCheckResourceAttrSet("streamkap_destination_snowflake.test", "id"),
 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "connector", "snowflake"),
+					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "auto_qa_dedupe_table_mapping.users", "JUNIT.USERS"),
+					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "auto_qa_dedupe_table_mapping.itst_scen20240528103635", "ITST_SCEN20240528103635"),
 				),
 			},
 			// Step 2: ImportState testing
@@ -127,54 +133,57 @@ resource "streamkap_destination_snowflake" "test" {
 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "cleanup_task_schedule", "60"),
 					resource.TestCheckResourceAttrSet("streamkap_destination_snowflake.test", "id"),
 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "connector", "snowflake"),
+					resource.TestCheckNoResourceAttr("streamkap_destination_snowflake.test", "auto_qa_dedupe_table_mapping"),
 				),
 			},
-// 			// Step 4: Update to remove passphrase (passphrase is None)
-// 			{
-// 				Config: providerConfig + `
-// variable "destination_snowflake_url_name" {
-// 	type        = string
-// 	description = "The URL name of the Snowflake database"
-// }
-// variable "destination_snowflake_private_key_nocrypt" {
-// 	type        = string
-// 	sensitive   = true
-// 	description = "The private key of the Snowflake database without passphrase"
-// }
-// resource "streamkap_destination_snowflake" "test" {
-// 	name                             = "test-destination-snowflake-no-passphrase"
-// 	snowflake_url_name               = var.destination_snowflake_url_name
-// 	snowflake_user_name              = "STREAMKAP_USER_JUNIT_NOCRYPT"
-// 	snowflake_private_key            = var.destination_snowflake_private_key_nocrypt
-// 	# snowflake_private_key_passphrase is omitted (None)
-// 	sfwarehouse                      = "STREAMKAP_WH"
-// 	snowflake_database_name          = "JUNIT"
-// 	snowflake_schema_name            = "JUNIT"
-// 	snowflake_role_name              = "STREAMKAP_ROLE_JUNIT"
-// 	ingestion_mode                   = "append"
-// }
-// `,
-// 				Check: resource.ComposeAggregateTestCheckFunc(
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "name", "test-destination-snowflake-no-passphrase"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_url_name", destinationSnowflakeURLName),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_user_name", "STREAMKAP_USER_JUNIT_NOCRYPT"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_private_key", destinationSnowflakePrivateKeyNoCrypt),
-// 					resource.TestCheckNoResourceAttr("streamkap_destination_snowflake.test", "snowflake_private_key_passphrase"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "sfwarehouse", "STREAMKAP_WH"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_database_name", "JUNIT"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_schema_name", "JUNIT"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_role_name", "STREAMKAP_ROLE_JUNIT"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "ingestion_mode", "append"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "hard_delete", "false"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "schema_evolution", "basic"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "use_hybrid_tables", "false"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "apply_dynamic_table_script", "false"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "dynamic_table_target_lag", "15"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "cleanup_task_schedule", "60"),
-// 					resource.TestCheckResourceAttrSet("streamkap_destination_snowflake.test", "id"),
-// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "connector", "snowflake"),
-// 				),
-// 			},
+			// 			// Step 4: Update to remove passphrase (passphrase is None)
+			// 			{
+			// 				Config: providerConfig + `
+			// variable "destination_snowflake_url_name" {
+			// 	type        = string
+			// 	description = "The URL name of the Snowflake database"
+			// }
+			// variable "destination_snowflake_private_key_nocrypt" {
+			// 	type        = string
+			// 	sensitive   = true
+			// 	description = "The private key of the Snowflake database without passphrase"
+			// }
+			// resource "streamkap_destination_snowflake" "test" {
+			// 	name                             = "test-destination-snowflake-no-passphrase"
+			// 	snowflake_url_name               = var.destination_snowflake_url_name
+			// 	snowflake_user_name              = "STREAMKAP_USER_JUNIT_NOCRYPT"
+			// 	snowflake_private_key            = var.destination_snowflake_private_key_nocrypt
+			// 	# snowflake_private_key_passphrase is omitted (None)
+			// 	sfwarehouse                      = "STREAMKAP_WH"
+			// 	snowflake_database_name          = "JUNIT"
+			// 	snowflake_schema_name            = "JUNIT"
+			// 	snowflake_role_name              = "STREAMKAP_ROLE_JUNIT"
+			// 	ingestion_mode                   = "append"
+			// }
+			// `,
+			// 				Check: resource.ComposeAggregateTestCheckFunc(
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "name", "test-destination-snowflake-no-passphrase"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_url_name", destinationSnowflakeURLName),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_user_name", "STREAMKAP_USER_JUNIT_NOCRYPT"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_private_key", destinationSnowflakePrivateKeyNoCrypt),
+			// 					resource.TestCheckNoResourceAttr("streamkap_destination_snowflake.test", "snowflake_private_key_passphrase"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "sfwarehouse", "STREAMKAP_WH"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_database_name", "JUNIT"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_schema_name", "JUNIT"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "snowflake_role_name", "STREAMKAP_ROLE_JUNIT"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "ingestion_mode", "append"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "hard_delete", "false"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "schema_evolution", "basic"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "use_hybrid_tables", "false"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "apply_dynamic_table_script", "false"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "dynamic_table_target_lag", "15"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "cleanup_task_schedule", "60"),
+			// 					resource.TestCheckResourceAttrSet("streamkap_destination_snowflake.test", "id"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "connector", "snowflake"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "auto_qa_dedupe_table_mapping.users", "JUNIT.USERS"),
+			// 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "auto_qa_dedupe_table_mapping.itst_scen20240528103635", "ITST_SCEN20240528103635"),
+			// 				),
+			// 			},
 			// Step 5: Update to add passphrase back (passphrase is not None)
 			{
 				Config: providerConfig + `
@@ -203,6 +212,10 @@ resource "streamkap_destination_snowflake" "test" {
 	snowflake_schema_name            = "JUNIT"
 	snowflake_role_name              = "STREAMKAP_ROLE_JUNIT"
 	ingestion_mode                   = "append"
+	auto_qa_dedupe_table_mapping = {
+		users                   = "JUNIT.USERS",
+		itst_scen20240528103635 = "ITST_SCEN20240528103635"
+	}
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
@@ -224,6 +237,8 @@ resource "streamkap_destination_snowflake" "test" {
 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "cleanup_task_schedule", "60"),
 					resource.TestCheckResourceAttrSet("streamkap_destination_snowflake.test", "id"),
 					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "connector", "snowflake"),
+					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "auto_qa_dedupe_table_mapping.users", "JUNIT.USERS"),
+					resource.TestCheckResourceAttr("streamkap_destination_snowflake.test", "auto_qa_dedupe_table_mapping.itst_scen20240528103635", "ITST_SCEN20240528103635"),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
