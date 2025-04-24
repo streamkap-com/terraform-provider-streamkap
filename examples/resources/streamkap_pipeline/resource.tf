9c47421a7ec9a99240ebab84542a2063bc37b151
Sau Phung <sau@streamkap.com>
2025-04-24 10:26:49 +0000

    feat: upate clickhouse mapping and pipeline docs

--- examples/resources/streamkap_pipeline/resource.tf
+++ examples/resources/streamkap_pipeline/resource.tf
@@ -30,22 +30,25 @@ variable "source_postgresql_password" {
 }
 
 resource "streamkap_source_postgresql" "example-source-postgresql" {
-  name                                      = "example-source-postgresql"
-  database_hostname                         = var.source_postgresql_hostname
-  database_port                             = 5432
-  database_user                             = "postgresql"
-  database_password                         = var.source_postgresql_password
-  database_dbname                           = "postgres"
-  database_sslmode                          = "require"
-  schema_include_list                       = "public"
-  table_include_list                        = "public.users,public.itst_scen20240528100603,public.itst_scen20240528103635,public.itst_scen20240530141046"
-  signal_data_collection_schema_or_database = "streamkap"
-  heartbeat_enabled                         = false
-  include_source_db_name_in_table_name      = false
-  slot_name                                 = "terraform_pgoutput_slot"
-  publication_name                          = "terraform_pub"
-  binary_handling_mode                      = "bytes"
-  ssh_enabled                               = false
+  name                                         = "example-source-postgresql"
+  database_hostname                            = var.source_postgresql_hostname
+  database_port                                = 5432
+  database_user                                = "postgresql"
+  database_password                            = var.source_postgresql_password
+  database_dbname                              = "postgres"
+  snapshot_read_only                           = "No"
+  database_sslmode                             = "require"
+  schema_include_list                          = "streamkap"
+  table_include_list                           = "streamkap.customer,streamkap.customer2"
+  signal_data_collection_schema_or_database    = "streamkap"
+  column_include_list                          = "streamkap[.]customer[.](id|name)"
+  heartbeat_enabled                            = false
+  heartbeat_data_collection_schema_or_database = null
+  include_source_db_name_in_table_name         = false
+  slot_name                                    = "terraform_pgoutput_slot"
+  publication_name                             = "terraform_pub"
+  binary_handling_mode                         = "bytes"
+  ssh_enabled                                  = false
 }
 
 variable "destination_snowflake_url_name" {
@@ -99,10 +102,8 @@ resource "streamkap_pipeline" "example-pipeline" {
     name      = streamkap_source_postgresql.example-source-postgresql.name
     connector = streamkap_source_postgresql.example-source-postgresql.connector
     topics = [
-      "public.itst_scen20240530141046",
-      "public.itst_scen20240528100603",
-      "public.itst_scen20240528103635",
-      "public.users",
+      "streamkap.customer",
+      "streamkap.customer2",
     ]
   }
   destination = {
