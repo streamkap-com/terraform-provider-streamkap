0aaa26b0db47a3a45d85dfb42dd7fd88e3a9d1a4
saustreamkap <135601087+saustreamkap@users.noreply.github.com>
2025-07-02 10:35:27 +0700

    chore: update latest mapping for dynamodb (#38)
    * chore: update latest mapping for dynamodb
    
    * generate doc

--- examples/resources/streamkap_source_dynamodb/resource.tf
+++ examples/resources/streamkap_source_dynamodb/resource.tf
@@ -31,15 +31,16 @@ resource "streamkap_source_dynamodb" "example-source-dynamodb" {
   aws_region                       = var.source_dynamodb_aws_region
   aws_access_key_id                = var.source_dynamodb_aws_access_key_id
   aws_secret_key                   = var.source_dynamodb_aws_secret_key
-  s3_export_bucket_name            = "streamkap-export"
-  table_include_list_user_defined  = "warehouse-test-2"
+  s3_export_bucket_name            = "tst-s3-export-snapshot"
+  table_include_list_user_defined  = "DynamoDBToClickHouseDemo"
   batch_size                       = 1024
   poll_timeout_ms                  = 1000
   incremental_snapshot_chunk_size  = 32768
   incremental_snapshot_max_threads = 8
-  incremental_snapshot_interval_ms = 8
   full_export_expiration_time_ms   = 86400000
   signal_kafka_poll_timeout_ms     = 1000
+  array_encoding_json              = true
+  struct_encoding_json             = true
 }
 
 output "example-source-dynamodb" {
