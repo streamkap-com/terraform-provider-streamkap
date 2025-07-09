f2fa8bc93265097ed662208a771a65d15ac52c86
saustreamkap <sau@streamkap.com>
2025-07-09 14:52:27 +0700

    add document

--- examples/resources/streamkap_pipeline/sqlserver_databricks/import.sh
+++ examples/resources/streamkap_pipeline/sqlserver_databricks/import.sh
@@ -1,2 +1,2 @@
-# Destination Snowflake can be imported by specifying the identifier.
+# Pipeline connecting SQL Server to Databricks can be imported by specifying the identifier
 terraform import streamkap_pipeline.example-pipeline 665e894ebb3753f38d983cee
